928b4aec63c7d960a9a2d087513f5cbca1a47721
Li WenJie <liwenjie@haylion.cn>
2018-12-17 18:00:16 +0800

    update annotation demo

--- demo-spring-mvc-annotation/README.md
+++ demo-spring-mvc-annotation/README.md
@@ -12,15 +12,46 @@
 - 组件激活： @EnableWebMvc(Spring 模块装配，替代配置文件)
 - 自定义组件： WebMvcConfigurer(Spring Bean)
 
+#### 注解配置：@Configuration (Spring 范式注解，模式注解)
+
+```java
+@Configuration
+```
+代表我是一个配置类，提供Bean
 
 
+#### 组件激活： @EnableWebMvc(Spring 模块装配，替代配置文件)
 
-### 自定义组件： WebMvcConfigurer(Spring Bean)
+- 启动模块注解，注入MVC模块需要的Bean，这是注解替代xml的启动注解，声明该注解，就会自动注入通用所需要的mvc配置bean
+如RequestMappingHandlerMapping等
 
-注解配置和组件激活比较简单，就不说了，自定义组件的话，我们可以通过实现WebMvcConfigurer接口去完成。
-也许，你还会想到一个叫WebMvcConfigurerAdapter的抽象类，不过这个抽象类在Spring 5+ 之后就被淘汰了。因为之后的WebMvcConfigurer使用Java 8 语法实现，
+- @EnableWebMvc的本质是将DelegatingWebMvcConfiguration类注入到Spring容器中，所以需要看具体实现，则通过该类查看
+
+#### 自定义组件： WebMvcConfigurer(Spring Bean)
+
+- 想要自定义组件的，我们可以通过实现WebMvcConfigurer接口去完成。
+- 也许，你还会想到一个叫WebMvcConfigurerAdapter的抽象类，不过这个抽象类在Spring 5+ 之后就被淘汰了。因为之后的WebMvcConfigurer使用Java 8 语法实现，
 接口可以有默认方法，然后再看看他们两者的代码，就会发现使用了java 1.8语法的WebMvcConfigurer和WebMvcConfigurerAdapter几乎一模一样，所以WebConfigurerAdapter就没有任何意义了。
 
+```java
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println("Interceptor....");
+                return true;
+            }
+        });
+
+    }
+}
+```
 
 
 
--- demo-spring-mvc-annotation/src/main/java/com/snailmann/spring/config/WebMvcConfig.java
+++ demo-spring-mvc-annotation/src/main/java/com/snailmann/spring/config/WebMvcConfig.java
@@ -4,18 +4,26 @@ package com.snailmann.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Spring MVC 配置类
+ *
+ * 当我们需要自定义组件的使用，就需要实现WebMvcConfigurer接口
+ *
  */
-
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 
     /**
@@ -36,4 +44,21 @@ public class WebMvcConfig {
 
     }
 
+    /**
+     * 这里是自定义了拦截器组件，注册到InterceptorRegistry中
+     * 然后我们debug DispatcherServlet就可以看到每一个handler对象就带有符合的拦截器引用
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println("Interceptor....");
+                return true;
+            }
+        });
+
+    }
 }
